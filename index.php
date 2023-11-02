0bcbc6de65da67b10750722a5021d3edc0657082
ajk3158 <71356356+ajk3158@users.noreply.github.com>
2023-11-02 15:51:35 -0400

    Fixed Database issue

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-// Sources used: https://cs4640.cs.virginia.edu, Published Site: https://cs4640.cs.virginia.edu/ajk5qwb/ADONIS/
+// Sources used: https://cs4640.cs.virginia.edu, Published Site: https://cs4640.cs.virginia.edu/ajk5qwb/ADONIS/ , Github: https://github.com/ajk3158/adonis
 // Author: Alex Kim, Jason Nguyen
 spl_autoload_register(function ($classname) {
     include "$classname.php";
